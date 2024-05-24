0187539a41bcb8cccc98ab920bae42668865ab03
Vidush <vidush23592@iiitd.ac.in>
2024-05-24 10:36:27 +0530

    new code in automtic key_detection

--- key_detection.py
+++ key_detection.py
@@ -1,20 +1,40 @@
-# import keyboard
+import keyboard
+import pyautogui
 
-# while True:
-#     if keyboard.read_key()==15:
-#         keyboard.read_key()
-#         keyboard.read_key()
-#         if keyboard.read_key()==14:
-#             print('ok pair')
-#             continue
+while True:
+    if keyboard.read_key()==15:
+        keyboard.read_key()
+        pyautogui.write(['space'])
+        pyautogui.click(196,216, clicks=1)
+        pyautogui.write(['right'])
 
-# # print(keyboard.read_key())
+        while (keyboard.read_key()!=14) & (keyboard.read_key()!=14):
+            pass
 
-import cv2
+    
+        pyautogui.write(['backspace'])
+        pyautogui.click(1136,383)
+        pyautogui.write(['space'])
+        continue
 
-vidcap = cv2.VideoCapture('/Users/teo/Desktop/summer_term/movies/the.outsiders.mp4')
-fps = vidcap.get(cv2.CAP_PROP_FPS)
-width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH )
-height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT )
-print(f"{fps} frames per second")
-print(f"{width} X {height} width X height")
+
+# import cv2
+
+# vidcap = cv2.VideoCapture('/Users/teo/Desktop/summer_term/movies/the.outsiders.mp4')
+# fps = vidcap.get(cv2.CAP_PROP_FPS)
+# width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH )
+# height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT )
+# print(f"{fps} frames per second")
+# print(f"{width} X {height} width X height")
+
+# import pyautogui,time
+
+# time.sleep(5)
+
+# pyautogui.doubleClick(540,560)
+# pyautogui.click(560,560)    
+# pyautogui.click(550,550)
+# pyautogui.click(550,570)
+
+
+
